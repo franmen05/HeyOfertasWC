271d264c5b898ef695ab72c502c1c8825e9745c6
Guillermo <Franmen05@gmail.com>
2015-08-24 12:15:57 -0400

    commit inicial

---
+++ HeyOfertasWC/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HeyOfertasWC.Models;
+using Newtonsoft.Json;
+
+namespace HeyOfertasWC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult EdealsByID(String _id)
+        {
+            var client = new RestClient("http://edeals.do:4000");
+            // client.Authenticator = new HttpBasicAuthenticator(username, password);
+
+            var request = new RestRequest("/api/deals/{id}", Method.GET);
+            request.AddUrlSegment("id", _id); // replaces matching token in request.Resource
+
+            
+            // execute the request
+            RestResponse<Deals> response = (RestResponse<Deals>)client.Execute<Deals>(request);
+            var content = response.Content; // raw content as string
+            var deal = JsonConvert.DeserializeObject<List<Deals>>(content)[0];
+
+            ViewBag.deal = deal;
+            ViewBag.des = deal.description;
+            ViewBag.fullurl = deal.source.url + deal.url;
+
+            return View();
+        }
+
+    }
+}
---
+++ HeyOfertasWC/Models/Deals.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace HeyOfertasWC.Models
+{
+    
+    public class Deals
+    {
+        
+        private string sdess;
+
+        public String _id { get; set; }
+
+        public String url { get; set; }
+
+        public string picture { get; set; }
+
+        public string description { get; set; }
+
+        public int originalPrice { get; set; }
+
+        public int discountedPrice { get; set; }
+
+        
+        public Source source { get; set; }
+
+    
+        public bool active { get; set; }
+
+    
+        public  int __v { get; set; }
+
+    
+        public string sdes {
+            get { return ""; }
+            set { this.sdess = value; }
+        }
+    }
+
+    
+    public class Source
+    {
+
+        public String name { get; set; }
+        public String url { get; set; }
+        
+    }
+}
